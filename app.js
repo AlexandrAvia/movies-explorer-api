9a87d7f2dc3fed019fc1fd68dc7bfbae8db8b3e6
Alexandr Semenov <sancho220394@gmail.com>
2022-10-01 13:06:19 +0300

    add cors

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const cors = require('cors');
-const helmet = require('helmet');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limiter = require('./middlewares/limiter');
-const router = require('./routes/index');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const cors = require("cors");
+const helmet = require("helmet");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const limiter = require("./middlewares/limiter");
+const router = require("./routes/index");
 
-const { PORT = 3001, MONGO = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3001, MONGO = "mongodb://localhost:27017/moviesdb" } =
+  process.env;
 const app = express();
 
 const options = {
   origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://api.diploma93.nomoredomains.sbs',
-    'https://api.diploma93.nomoredomains.sbs',
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://api.diploma93.nomoredomains.sbs",
+    "https://api.diploma93.nomoredomains.sbs",
+    "http://diploma93.nomoredomains.sbs",
+    "https://diploma93.nomoredomains.sbs",
   ],
   credentials: true,
 };
@@ -25,13 +28,13 @@ const options = {
 mongoose.connect(MONGO);
 
 app.use(requestLogger);
-app.use('*', cors(options));
+app.use("*", cors(options));
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 app.use(helmet());
 app.use(limiter);
-app.use('/', router);
+app.use("/", router);
 
 app.use(errorLogger);
 app.use(errors());
@@ -39,7 +42,7 @@ app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode, message } = err;
   if (!statusCode) {
-    res.status(500).send({ message: 'Внутрення ошибка сервера' });
+    res.status(500).send({ message: "Внутрення ошибка сервера" });
   }
   res.status(statusCode).send({ message });
   next();
